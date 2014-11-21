5339c6c53703b5c2b285b791fe7d59e83ca707f7
Thomas O'Donnell <andy.tom@gmail.com>
2014-11-21 20:52:37 +0000

    Inital commit

---
+++ content/pages/about.md
@@ -0,0 +1,5 @@
+Title: About
+Date: 2012-11-21 19:48
+
+A simple blog that contains write ups of simple projects I am currently
+working on.
---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = "andytom"
+SITENAME = 'andytom.github.io'
+#SITEURL = 'https://andytom.github.io'
+SITEURL = ''
+THEME = "./pelican-themes/gum"
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
